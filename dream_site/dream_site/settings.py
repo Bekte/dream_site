3d80622244566287b6c9088b5a1b9ae5bd48c87c
Bekte <ainagulbektemirova17@gmail.com>
2022-12-24 17:11:51 +0200

    templates new

--- dream_site/dream_site/settings.py
+++ dream_site/dream_site/settings.py
@@ -113,13 +113,12 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
+# Static files (css, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = []
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
